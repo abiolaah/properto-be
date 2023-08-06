e72768bedb1dd608a7345845af21307698300be2
Elif B.Canatan <113218981+ElifBC@users.noreply.github.com>
2023-08-06 11:15:03 -0400

    Update house.js

--- properto-be/server/routes/house.js
+++ properto-be/server/routes/house.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { displayHouseList, processAddPage } from '../controllers/house.js';
+import houseModel from '../models/house.js';
 
 const router = express.Router();
 
@@ -11,3 +12,28 @@ router.post('/houses', processAddPage);
 
 export default router;
 
+
+// PUT route for updating a house
+router.put('/houses/:id', async (req, res) => {
+    try {
+        const updatedHouse = await houseModel.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        res.json({ house: updatedHouse });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: err });
+    }
+});
+// GET route for getting details of a property by ID
+router.get('/houses/:id', async (req, res) => {
+    try {
+        const house = await houseModel.findById(req.params.id);
+        if (house) {
+            res.json({ house });
+        } else {
+            res.status(404).json({ error: 'House not found' });
+        }
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: err });
+    }
+});
